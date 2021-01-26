53447a7b381392007372d8750697a85a6c4221e3
yoshzawa <yoshzawa@gmail.com>
2021-01-27 06:52:40 +0900

    Test01,06をパス、09は作成途中

---
+++ src/AllTests.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	Kadai01Test.class,
+	Kadai06Test.class,
+	Kadai09Test.class})
+public class AllTests {
+
+}
--- src/Kadai01Test.java
+++ src/Kadai01Test.java
@@ -2,10 +2,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
 
-class Kadai01Test {
+public class Kadai01Test {
 
 	@Test
-	void testReverseString() {
+	public void testReverseString() {
 		Kadai01 k = new Kadai01();
 		assertNull(k.ReverseString(null));
 		assertEquals("4321",k.ReverseString("1234"));
